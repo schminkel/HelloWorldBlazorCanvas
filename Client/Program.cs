7a5fd41abf5a81f2c9eff3301c7230fe785aadb4
Thorsten Schminkel <thorsten@schminkel.de>
2022-01-20 11:06:03 +0100

    init

--- Client/Program.cs
+++ Client/Program.cs
@@ -7,14 +7,11 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Blazor.Extensions.Canvas.Canvas2D;
-using Blazor.Extensions;
 
 namespace HelloWorldBlazorCanvas.Client
 {
     public class Program
     {
-
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
